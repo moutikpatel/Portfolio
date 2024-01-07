abc3347296be74380cc875c683a1064f1fcf9269
Moutik Patel <85564675+moutikpatel@users.noreply.github.com>
2024-01-06 18:11:04 -0700

    Final Commit

---
+++ src/constants/index.js
@@ -0,0 +1,255 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "IT Support & Hyflex Ambassador",
+        company_name: "Seneca College",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "May 2023 - December 2023",
+        points: [
+            "Assisted professors with IT-related problems",
+            "Installed/upgraded computer workstations campus-wide",
+            "Researched and proposed new classroom technologies",
+            "Re-imaged machines for updates and troubleshooting",
+           
+        ],
+    },
+    {
+        title: "Desktop Support Technician",
+        company_name: "Teck Resources",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "September 2022 - May 2023",
+        points: [
+            "Provided technical support to users, troubleshooted back-end server and network connectivity issues, escalating requests to level 3 support resources as appropriate using ServiceNow to both IT and OT Systems. Monitor the queue to prioritize and address high priority and critical issues first. Participated in incident, tasks, configurations",
+            "Provided onsite desktop and deployment support with team which included managing, supporting, and monitoring Microsoft 365 and Azure, Microsoft Endpoint Manager and Azure Intune",
+            "Helped internal teams and Contractors to test and diagnose instrument on Biggest Haul Trucks in the world and more, with safety and precautions in mind",
+            "Helped Autonomous Haulage System in configuration and diagnosis",
+         
+        ],
+    },
+    {
+        title: "Sales Associate",
+        company_name: "7-Eleven",
+        icon: shopify,
+        iconBg: "#b7e4c7",
+        date: "July 2021 - September 2022",
+        points: [
+            "• Developed expert time-management and multi-tasking skills to ensure the workplace set for customer satisfaction. Resolved customer concerns using strong communication and conflict management skills",
+            "Answered customer requests, provided store information, and escorted to desired store areas",
+            "Used feedback from customers and personal observations to better capitalize on sales opportunities",
+        ],
+    },
+    {
+        title: "IT Support Technologist",
+        company_name: "Bank Of India",
+        icon: meta,
+        iconBg: "#a2d2ff",
+        date: "December 2020 - May 2021",
+        points: [
+            "Testing of IT systems and experience with Computer support",
+            "Imaging, configuring, and installing systems issued to users, decommission retired systems",
+            "Updating inventory records via IT Asset management system",
+            "Installing, Managing, and Configuration Windows Operating System",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/moutikpatel',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/moutikpatel',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'GrabSmartly',
+        description: 'Developed a cross-platform mobile application that simplifies grocery shopping and meal planning. The app combines the functionalities of a grocery list organizer, recipe ingredient selector, and real-time price comparison tool, all in one convenient platform.',
+        link: 'https://github.com/moutikpatel/GrabSmartly',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Pokedex',
+        description: '"Your go-to online Pokédex for quick and easy access to comprehensive Pokémon information."',
+        link: 'https://github.com/moutikpatel/Pokedex',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Robotic - Autonomous Navigation Improvement',
+        description: 'Designed and built a ROS model in Gazebo to simulate a robot that can autonomously navigate a maze and avoid obstacles.',
+        link: 'https://github.com/moutikpatel/ROS-Autonomous-Navigation',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: '3D-Fun',
+        description: 'Fun 3d webpage with interactive models and animations using Three.js and React.js.',
+        link: 'https://github.com/moutikpatel/3D-Fun',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Real-Estate Application',
+        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+        link: 'https://github.com/moutikpatel/RealEstateMarketPlace',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'AI News Application',
+        description: 'App that leverages AI to read news interact with human and can read headlines of latest news using ALAN AI.',
+        link: 'https://github.com/moutikpatel/AINewsReader',
+    }
+];
+
+
+export const education = [
+    {
+        title: "Advanced Diploma in Computer Programming & Analysis",
+        company_name: "Seneca College",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "May 2021 - December 2023",
+        points: [
+            "Throughout the advanced diploma program, I have engaged in an in-depth exploration of various facets of computer programming. The curriculum encompasses a diverse range of subjects, including advanced programming languages, software architecture, system analysis, and database management. The emphasis on hands-on learning has been invaluable, allowing me to apply theoretical knowledge to real-world scenarios and projects.",
+        ],
+    },
+    {
+        title: "Diploma in Information Technology",
+        company_name: "MS University Baroda",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "September 2017 - September 2020",
+        points: [
+            "This program typically covers a broad range of topics, including computer systems, programming languages, software development, network administration, database management, and cybersecurity",
+            "Throughout the course, I learn to analyze IT problems, design effective solutions, and implement them using various technologies. Practical hands-on experience is often a key component, allowing us to develop proficiency in using IT tools and systems",
+            "By the end of the diploma program, I was equipped with a solid foundation in IT concepts, problem-solving abilities, and the technical skills necessary to pursue careers in areas such as software development, IT support, system administration, and network management.",
+        ],
+    },
+];
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,123 @@
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
+import Footer from "../components/Footer";
+
+
+
+
+const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [loading, setLoading] = useState(false);
+
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Moutik Patel",
+          from_email: form.email,
+          to_email: "moutikpatel@gmail.com",
+          message: "Message from Email: " + form.email + " \n Message:" + form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+        setLoading(false);
+        setForm({ name: "", email: "", message: "" });
+        }
+      );
+  };
+
+  return (
+    <section className='relative flex lg:flex-row flex-col max-container'>
+
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text'>Get in Touch</h1>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className='w-full flex flex-col gap-7 mt-14'
+        >
+          <label className='text-black-500 font-semibold'>
+            Name
+            <input
+              type='text'
+              name='name'
+              className='input'
+              placeholder='John'
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Email
+            <input
+              type='email'
+              name='email'
+              className='input'
+              placeholder='John@gmail.com'
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Your Message
+            <textarea
+              name='message'
+              rows='4'
+              className='textarea'
+              placeholder='Write your thoughts here...'
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <button
+            type='submit'
+            disabled={loading}
+            className='btn'
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {loading ? "Sending..." : "Submit"}
+          </button>
+        </form>
+
+        <hr className='border-slate-200' />
+
+
+<Footer/>
+
+      </div>
+
+
+      
+    </section>
+  );
+};
+
+export default Contact;
